74ab0adaaaf048bdcb712a557df1e99dc34302d3
Isabella DeCocco <decoccoisabella@gmail.com>
2019-02-17 20:51:16 -0500

    puppeteer with scroll

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,26 +1,30 @@
 import express from "express";
+import { CLIENT_RENEG_WINDOW } from "tls";
 const app = express();
-const scraper = require("image-scraper");
+const puppeteer = require("puppeteer");
 const cors = require("cors");
 app.use(cors());
-app.get("/test", (req, res) => {
-    console.log("helpp");
-    res.send("ok");
-});
-const imgArray: any[] = [];
-app.get("/scrape/:URL", (req, res) => {
-    try {
-        const URL = decodeURIComponent(req.params.URL);
-        const Scraper = new scraper(req.params.URL);
-        Scraper.on("image", function (image: any) {
-            if (imgArray.includes(image.address) === false) {
-                imgArray.push(image.address);
+
+let window: any;
+const imgArr: any[] = [];
+
+(async () => {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+
+    await page.goto('https://githuhttps://www.archdaily.com/category/galleryb.com');
+    await page.evaluate( async () => {
+        await window.scrollBy(0, window.innerHeight);
+        window.on("image", (image: any) => {
+            if(imgArr.includes(image.address) === false) {
+                imgArr.push(image.address);
             }
-        })
-        Scraper.on("end", () => res.send(imgArray));
-        Scraper.scrape();
-    }
-    catch (err) {
-    }
-});
+        });
+
+    });
+
+    browser.close();
+    return imgArr;
+})();
+
 app.listen(8000);
